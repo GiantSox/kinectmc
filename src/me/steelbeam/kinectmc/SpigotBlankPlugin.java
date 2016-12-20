f4194278ba6bca32fb7deaef91edcb24e654a41f
BuildTools <GiantSox@users.noreply.github.com>
2016-12-19 23:18:54 -0800

    who knows

--- src/me/steelbeam/kinectmc/SpigotBlankPlugin.java
+++ src/me/steelbeam/kinectmc/SpigotBlankPlugin.java
@@ -1,13 +1,21 @@
 package me.steelbeam.kinectmc;
 import org.bukkit.Bukkit;
+
+import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player.*;
+
 
 public class SpigotBlankPlugin extends JavaPlugin {
 	@Override
 	public void onEnable()
 	{
 		Bukkit.getConsoleSender().sendMessage("KinectMC Initialized!");
+		
+		
 	}
 	
 	@Override
@@ -15,5 +23,15 @@ public class SpigotBlankPlugin extends JavaPlugin {
 	{
 		
 	}
+	
+	@EventHandler
+	public void onStep(PlayerMoveEvent pme)
+	{
+		/*Player player = pme.getPlayer();
+		Location locUnderPlayer = player.getLocation();
+		Block blawk = locUnderPlayer.getBlock();*/
+		Bukkit.getConsoleSender().sendMessage(pme.getPlayer().getLocation().getX());
+	}
+	
 
 }
